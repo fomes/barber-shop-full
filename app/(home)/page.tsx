da7aa5dbbeb2a5e8310b1fd00d400d9942b20e4d
fomes <fgomesdeluna@gmail.com>
2024-01-31 15:20:05 -0300

    feat: add initial barbershop item

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,18 @@
-export const revalidate = 0;
-
+import BarbershopItem from "@/components/barbershop-item";
 import BookingItem from "@/components/booking-item";
 import Header from "@/components/header";
 import Search from "@/components/search";
+import { db } from "@/lib/prisma";
+import { Barbershop } from "@prisma/client";
 import dayjs from "dayjs";
 
 import "dayjs/locale/pt-br";
 
-export default function Home() {
-  const today = dayjs().format("dddd, DD [de] MMMM");
+export default async function Home() {
+  const barbershops = await db.barbershop.findMany({});
 
   dayjs.locale("pt-br");
+  const today = dayjs().format("dddd, DD [de] MMMM");
 
   return (
     <main>
@@ -28,6 +30,30 @@ export default function Home() {
       <div className="px-5 mt-6">
         <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold"></h2>
         <BookingItem />
+      </div>
+
+      <div className="mt-6">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Recomendados
+        </h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop: Barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Populares
+        </h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop: Barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </main>
   );
