f1865d7b941256718e3f5a74b74726da5500ffbc
fomes <fgomesdeluna@gmail.com>
2024-01-31 14:47:09 -0300

    feat: add initial book item component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,10 @@
+export const revalidate = 0;
+
+import BookingItem from "@/components/booking-item";
 import Header from "@/components/header";
 import Search from "@/components/search";
 import dayjs from "dayjs";
+
 import "dayjs/locale/pt-br";
 
 export default function Home() {
@@ -19,6 +23,11 @@ export default function Home() {
 
       <div className="px-5 pt-5">
         <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold"></h2>
+        <BookingItem />
       </div>
     </main>
   );
